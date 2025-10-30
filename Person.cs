02a7d5d2d7d5aca60329d08017193c12e6eee2d3
maribel <maribelelmann024@gmail.com>
2025-10-30 03:01:35 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseTask;
+
+public partial class Person
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Gender { get; set; }
+}
---
+++ StoreProcContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask;
+
+public partial class StoreProcContext : DbContext
+{
+    public StoreProcContext()
+    {
+    }
+
+    public StoreProcContext(DbContextOptions<StoreProcContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Person");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsFixedLength();
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
